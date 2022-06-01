94c81687f9ba8a15d03e609ec588703981b1a5f0
GermanParra <gerpar13@gmail.com>
2022-06-01 16:37:02 -0400

    Update

--- Deep_Learning_Apps_with_Keras/Exercise Files/03/preprocess_data.py
+++ Deep_Learning_Apps_with_Keras/Exercise Files/03/preprocess_data.py
@@ -2,21 +2,25 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 # Load training data set from CSV file
-training_data_df =
+training_data_df = pd.read_csv("sales_data_training.csv")
 
 # Load testing data set from CSV file
-test_data_df =
+test_data_df = pd.read_csv("sales_data_test.csv")
 
 # Data needs to be scaled to a small range like 0 to 1 for the neural
 # network to work well.
-scaler =
+scaler = MinMaxScaler(feature_range=(0,1))
 
 # Scale both the training inputs and outputs
-scaled_training =
-scaled_testing =
+scaled_training = scaler.fit_transform(training_data_df)
+scaled_testing = scaler.transform(test_data_df)
 
 # Print out the adjustment that the scaler applied to the total_earnings column of data
-print("Note: total_earnings values were scaled by multiplying by {:.10f} and adding {:.6f}".format(scaler.scale_[8], scaler.min_[8]))
+print("Note: total_earnings values were scaled by multiplying by {:.10f} and adding {:.6f}".format(
+                                                                                                    scaler.scale_[8],
+                                                                                                    scaler.min_[8]
+                                                                                                   )
+      )
 
 # Create new pandas DataFrame objects from the scaled data
 scaled_training_df = pd.DataFrame(scaled_training, columns=training_data_df.columns.values)
@@ -25,3 +29,4 @@ scaled_testing_df = pd.DataFrame(scaled_testing, columns=test_data_df.columns.va
 # Save scaled data dataframes to new CSV files
 scaled_training_df.to_csv("sales_data_training_scaled.csv", index=False)
 scaled_testing_df.to_csv("sales_data_test_scaled.csv", index=False)
+
