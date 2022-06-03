ee7eeae1b2a4f9e4ee3ee84441d4bf1711d5710a
GermanParra <gerpar13@gmail.com>
2022-06-02 23:00:36 -0400

    NNs with Keras-Tensorflow

--- Deep_Learning_Apps_with_Keras/Exercise Files/04/load_saved_model.py
+++ Deep_Learning_Apps_with_Keras/Exercise Files/04/load_saved_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from keras.models import load_model
 
-model =
+model = load_model("trained_model.h5")
 
 X = pd.read_csv("proposed_new_product.csv").values
 prediction = model.predict(X)
@@ -11,7 +11,7 @@ prediction = prediction[0][0]
 
 # Re-scale the data from the 0-to-1 range back to dollars
 # These constants are from when the data was originally scaled down to the 0-to-1 range
-prediction = prediction + 0.1159
+prediction = prediction + 0.115913
 prediction = prediction / 0.0000036968
 
 print("Earnings Prediction for Proposed Product - ${}".format(prediction))
--- Deep_Learning_Apps_with_Keras/Exercise Files/05/image_recognition.py
+++ Deep_Learning_Apps_with_Keras/Exercise Files/05/image_recognition.py
@@ -1,27 +1,27 @@
 import numpy as np
-from keras.preprocessing import image
-from keras.applications import resnet50
+from keras import utils
+from keras.applications import resnet
 
 # Load Keras' ResNet50 model that was pre-trained against the ImageNet database
-model =
+model = resnet.ResNet50()
 
 # Load the image file, resizing it to 224x224 pixels (required by this model)
-img =
+img = utils.load_img("bay.jpg", target_size=(224, 224))
 
 # Convert the image to a numpy array
-x =
+x = utils.img_to_array(img)
 
 # Add a forth dimension since Keras expects a list of images
-x =
+x = np.expand_dims(x, axis=0)
 
 # Scale the input image to the range used in the trained network
-x =
+x = resnet.preprocess_input(x)
 
 # Run the image through the deep neural network to make a prediction
-predictions =
+predictions = model.predict(x)
 
 # Look up the names of the predicted classes. Index zero is the results for the first image.
-predicted_classes =
+predicted_classes = resnet.decode_predictions(predictions, top=10)
 
 print("This is an image of:")
 
