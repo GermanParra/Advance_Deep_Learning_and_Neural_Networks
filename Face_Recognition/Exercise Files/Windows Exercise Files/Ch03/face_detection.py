04909fa175a70b23d7c0bfac65ed68ebcfa52e67
GermanParra <gerpar13@gmail.com>
2022-06-15 19:12:39 -0400

    face_location

--- Face_Recognition/Exercise Files/Windows Exercise Files/Ch03/face_detection.py
+++ Face_Recognition/Exercise Files/Windows Exercise Files/Ch03/face_detection.py
@@ -1,12 +1,13 @@
+from turtle import right
 import PIL.Image
 import PIL.ImageDraw
-
+import face_recognition
 
 # Load the jpg file into a numpy array
-
+image = face_recognition.load_image_file('Exercise Files\Windows Exercise Files\Ch03\people.jpg')
 
 # Find all the faces in the image
-
+face_locations = face_recognition.face_locations(image)
 
 number_of_faces = len(face_locations)
 print("I found {} face(s) in this photograph.".format(number_of_faces))
@@ -17,7 +18,7 @@ pil_image = PIL.Image.fromarray(image)
 for face_location in face_locations:
 
     # Print the location of each face in this image. Each face is a list of co-ordinates in (top, right, bottom, left) order.
-
+    top, right, bottom, left = face_location
     print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
 
     # Let's draw a box around the face
