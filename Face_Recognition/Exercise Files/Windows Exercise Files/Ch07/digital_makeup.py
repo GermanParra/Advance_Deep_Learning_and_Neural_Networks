073d571b0560af7b1662856f00530581fdc43da2
GermanParra <gerpar13@gmail.com>
2022-06-22 08:00:28 -0400

    drawing over faces

--- Face_Recognition/Exercise Files/Windows Exercise Files/Ch07/digital_makeup.py
+++ Face_Recognition/Exercise Files/Windows Exercise Files/Ch07/digital_makeup.py
@@ -2,10 +2,10 @@ from PIL import Image, ImageDraw
 import face_recognition
 
 # Load the jpg file into a numpy array
-image =
+image = face_recognition.load_image_file("Exercise Files\Windows Exercise Files\Ch07\people.jpg")
 
 # Find all facial features in all the faces in the image
-face_landmarks_list =
+face_landmarks_list = face_recognition.face_landmarks(image)
 
 # Load the image into a Python Image Library object so that we can draw on top of it and display it
 pil_image = Image.fromarray(image)
@@ -18,13 +18,12 @@ for face_landmarks in face_landmarks_list:
     #  - chin, left_eyebrow, right_eyebrow, nose_bridge, nose_tip, left_eye, right_eye, top_lip, bottom_lip
 
     # Draw a line over the eyebrows
-
-
+    d.line(face_landmarks['left_eyebrow'], fill=(0, 0, 0, 100), width=3)
+    d.line(face_landmarks['right_eyebrow'], fill=(0, 0, 0, 100), width=3)
 
     # Draw over the lips
-
-
-
+    d.polygon(face_landmarks['top_lip'], fill=(128, 0, 128, 100))
+    d.polygon(face_landmarks['bottom_lip'], fill=(128, 0, 128, 100))
 
 # Show the final image
 pil_image.show()
